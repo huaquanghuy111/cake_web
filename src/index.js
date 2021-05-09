ffe9d62b2d919c41d9356b930ae77cc481dd8437
huaquanghuy111 <75260164+huaquanghuy111@users.noreply.github.com>
2021-05-09 22:17:37 +0700

    change port

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const express = require('express')
 const db = require('./config/db')
 db.connect();
 const app = express()
-const port = 3000;
+const port = process.env.PORT || 3000;
 const path = require('path')
 const exphbs  = require('express-handlebars');
 app.engine('hbs', exphbs({extname:'.hbs'}));
